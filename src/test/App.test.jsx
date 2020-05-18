f98bb86c6ef721bbe1c06c6c9a896bc014432687
Daniel Laloush <d.laloush@outlook.com>
2020-05-18 13:30:54 -0400

    fixin erros

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -4,6 +4,6 @@ import App from '../component/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/AC/i);
   expect(linkElement).toBeInTheDocument();
 });
